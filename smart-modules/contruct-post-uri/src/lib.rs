b34651e8944eab8af8260cbdb68cd3774fdab71c
Alex Evert <alex@aleeve.dev>
2025-10-06 09:10:01 +0200

    Add uri extractor smartmodule

---
+++ smart-modules/contruct-post-uri/src/lib.rs
@@ -0,0 +1,42 @@
+use eyre::eyre;
+use fluvio_smartmodule::{RecordData, Result, SmartModuleRecord, smartmodule};
+use serde_json::{Map, Value};
+use std::str::FromStr;
+
+#[smartmodule(map)]
+pub fn map(record: &SmartModuleRecord) -> Result<(Option<RecordData>, RecordData)> {
+    let key = record.key.clone();
+
+    let string = std::str::from_utf8(record.value.as_ref())?;
+    let mut value: Value = serde_json::from_str(string)?;
+    let obj = value
+        .as_object_mut()
+        .ok_or(eyre!("Failed to parse value"))?;
+
+    let uri = get_uri(obj)?;
+    let uri_value = Value::String(uri);
+    obj.insert("uri".to_string(), uri_value);
+
+    Ok((key, value.to_string().as_str().into()))
+}
+
+fn get_uri(obj: &Map<String, Value>) -> Result<String> {
+    let did = obj
+        .get("did")
+        .and_then(|v| v.as_str())
+        .ok_or(eyre!("did missing or not a string"))?;
+
+    let commit = obj.get("commit").ok_or(eyre!("commit missing"))?;
+
+    let collection = commit
+        .get("collection")
+        .and_then(|v| v.as_str())
+        .ok_or(eyre!("commit.collection missing or not a string"))?;
+
+    let rkey = commit
+        .get("rkey")
+        .and_then(|v| v.as_str())
+        .ok_or(eyre!("commit.rkey missing or not a string"))?;
+
+    Ok(format!("at://{did}/{collection}/{rkey}"))
+}
