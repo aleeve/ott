abd9844e84d6d1e25cb87363581fd83d5bc368c7
Alex Evert <alex@aleeve.dev>
2025-10-06 09:10:01 +0200

    Fix name typo

--- hooks.sh
+++ hooks.sh
@@ -1,6 +1,6 @@
 fluvio cluster status || fluvio cluster start --k8
 cd ./smart-modules/assign-record-key/ && smdk build && smdk load
 cd -
-cd ./smart-modules/contruct-post-uri/ && smdk build && smdk load
+cd ./smart-modules/construct-post-uri/ && smdk build && smdk load
 cd -
 kind load docker-image loaded-pg:$POSTGRES_VERSION
