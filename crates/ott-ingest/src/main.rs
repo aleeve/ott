203a8c1caba5d9d5ebfaad0d6eebeff9aa35b941
Alex Evert <alex@aleeve.dev>
2025-10-06 09:10:01 +0200

    Adjust ingestor

--- crates/ott-ingest/src/main.rs
+++ crates/ott-ingest/src/main.rs
@@ -1,10 +1,11 @@
+use serde::Deserialize;
 use tokio::{
     select,
     time::{Duration, sleep},
 };
 use tokio_stream::StreamExt;
 use tracing::{debug, error, info, warn};
-use tracing_subscriber::{EnvFilter, fmt};
+use tracing_subscriber::{EnvFilter, field::display, fmt};
 
 use fluvio::{
     Fluvio, Offset,
@@ -14,22 +15,26 @@ use moka::{
     ops::compute::{CompResult, Op},
     sync::Cache,
 };
-use ott_ingest::at_types;
 use ott_ingest::tei_client;
 
 const LIKES_TOPIC: &str = "raw-likes";
 const POSTS_TOPIC: &str = "raw-posts";
 const PARTITION_NUM: u32 = 0;
 
-#[derive(Clone)]
-struct BskyPost {
-    likes: u32,
-    uri: String,
+#[derive(Debug, Deserialize, Clone)]
+struct Post {
     did: String,
-    text: String,
+    uri: String,
+    #[serde(default)]
+    count: u32,
 }
 
-impl BskyPost {}
+#[derive(Debug, Deserialize, Clone)]
+struct Like {
+    did: String,
+    #[serde(rename = "commit.record.subject.uri")]
+    uri: String,
+}
 
 #[tokio::main]
 async fn main() {
@@ -38,7 +43,7 @@ async fn main() {
         .with_env_filter(EnvFilter::from_default_env())
         .init();
 
-    let posts_cache: Cache<String, u32> = Cache::builder()
+    let posts_cache: Cache<String, Post> = Cache::builder()
         .time_to_live(Duration::from_secs(60 * 60))
         .build();
 
@@ -72,36 +77,27 @@ async fn main() {
         let pcc = posts_cache.clone();
         let lcc = posts_cache.clone();
         select! {
-            result = like_stream.next() => {
-                match result {
-                    Some(Ok(record)) => {
-                        record.value();
-                        pcc.entry(String::from("hej"))
-                            .and_compute_with(|maybe_entry| {
-                                if let Some(entry) = maybe_entry {
-                                    let counter = entry.into_value();
-                                    if counter < 2 {
-                                        Op::Put(counter.saturating_add(1)) // Update
-                                    } else {
-                                        Op::Remove
-                                    }
-                                } else {
-                                    Op::Put(1) // Insert
-                                }
-                        });
-                    }
-                    Some(Err(e)) => {todo!()}
-                    None => {todo!()}
-                }
-            }
-
-            Some(Ok(result)) = posts_stream.next() => {
-                lcc.entry(String::from("hej"))
+            Some(Ok(record)) = posts_stream.next() => {
+                let post: Post = serde_json::from_slice(record.value()).unwrap();
+                pcc.entry(post.uri.clone())
+                    .and_compute_with(|maybe_entry| {
+                        if maybe_entry.is_some() {
+                            Op::Nop
+                        } else {
+                            Op::Put(post) // Insert
+                        }
+                });
+            },
+            Some(Ok(record)) = like_stream.next() => {
+                let like: Like = serde_json::from_slice(record.value()).unwrap();
+                lcc.entry(like.uri)
                     .and_compute_with(|maybe_entry| {
                         if let Some(entry) = maybe_entry {
-                            let counter = entry.into_value();
-                            if counter < 2 {
-                                Op::Put(counter.saturating_add(1)) // Update
+                            let mut post = entry.into_value();
+                            if post.count < 2 {
+                                post.count +=1;
+                                warn!("Incread counter for {:#?}", post);
+                                Op::Put(post)
                             } else {
                                 Op::Remove
                             }
