e2c524b11597e8f2e9a51ef21b50ee0225e6a75e
Alex Evert <alex@aleeve.dev>
2025-10-10 11:01:43 +0200

    Start TEI in pre deploy hook

--- hooks.sh
+++ hooks.sh
@@ -4,3 +4,18 @@ cd -
 cd ./smart-modules/construct-post-uri/ && smdk build && smdk load
 cd -
 kind load docker-image loaded-pg:$POSTGRES_VERSION
+
+echo "Checking if the embeddings-server is running at port 8080"
+if ! curl -s -o /dev/null localhost:8080; then
+  echo "Starting TEI"
+  export model=jinaai/jina-embeddings-v2-base-en
+  text-embeddings-router --model-id $model --port 8080 --auto-truncate &
+
+  # Wait until the service is up
+  while ! curl -s -o /dev/null localhost:8080; do
+    echo "Waiting for TEI to start..."
+    sleep 2
+  done
+
+  echo "TEI is up!"
+fi
